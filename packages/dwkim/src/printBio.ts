19e3727299bdadc5384a1bf89789e690fbb606fc
domuk-k <dannyworks102@gmail.com>
2025-06-21 14:56:54 +0900

    refactor: 프로필 정보 구조 변경 및 출력 함수 수정
    - profile.json 파일 삭제 및 프로필 정보를 printBio.ts 파일 내에 상수로 정의
    - printBio 함수를 printProfile로 이름 변경하고, profile 인자를 제거하여 내부에서 직접 프로필 정보를 사용하도록 수정

--- packages/dwkim/src/printBio.ts
+++ packages/dwkim/src/printBio.ts
@@ -1,19 +1,10 @@
 import boxen, { type Options } from 'boxen';
 import chalk from 'chalk';
 
-interface Profile {
-  name: string;
-  email: string;
-  title: string;
-  github: string;
-  website: string;
-  bio: string;
-  quote: string;
-}
 /**
  * 명함 출력 함수 - 프로필 정보를 기반으로 명함을 출력합니다.
  */
-export function printBio(profile: Profile) {
+export function printProfile() {
   const boxenOptions: Options = {
     padding: 1,
     margin: 0,
@@ -28,9 +19,22 @@ export function printBio(profile: Profile) {
     `${chalk.gray.greenBright('Email')} ${profile.email}`,
     `${chalk.gray.greenBright('GitHub')} ${profile.github}`,
     `${chalk.gray.greenBright('Website')} ${profile.website}`,
+    `${chalk.gray.greenBright('Project')} ${profile.project}`,
     '',
     chalk.white(profile.quote),
   ].join('\n');
 
   console.log(boxen(cardContent, boxenOptions));
 }
+
+export const profile = {
+  id: '1',
+  name: '김동욱',
+  email: 'dannyworks102@gmail.com',
+  title: 'Software Engineer, BHSN.ai',
+  github: 'https://github.com/domuk-k',
+  website: 'https://domuk-k.vercel.app',
+  project: 'https://github.com/domuk-k/dwkim',
+  bio: 'Problem Solver 🤹, Marathon Runner 🏃, Opensource committer 💻, casual Yogi 🧘',
+  quote: 'Customer Centric, Focus on what you can control',
+} as const;
